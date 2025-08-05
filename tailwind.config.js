f860e014ac18a7b6b9369edb7faa4bd729d8b702
vanajmoorthy <vanajmoorthy@gmail.com>
2025-08-06 04:40:22 +0530

    feat: set up tailwind, improve ui, get analytics engine working

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // This is the only thing the config file needs.
+  // It tells Tailwind where to look for class names.
+  content: [
+    './core/templates/**/*.html'
+  ],
+}
