611781173293637d1ccd99aa2ae9a73bad1e1ca6
vanajmoorthy <vanajmoorthy@gmail.com>
2025-08-31 15:41:48 +0100

    deploy

--- bibliotype/settings.py
+++ bibliotype/settings.py
@@ -132,6 +132,12 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
